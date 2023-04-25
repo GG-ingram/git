c3737ea00f2936a9975444333ed3a855474e5b44
root <root@qq.com>
2023-04-25 17:25:11 +0800

    xiugai

--- src/main/java/com/henu/git/GitTest.java
+++ src/main/java/com/henu/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     private Integer id;
     private String email;
     void test(){
+        System.out.println("hot-fix");
         System.out.println(569);
         System.out.println(123);
         System.out.println(1233);
