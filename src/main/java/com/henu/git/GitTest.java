b4edb6f40b6686bc0bc4e012f206b53e00c8673b
root <root@qq.com>
2023-04-25 16:21:04 +0800

    hot-fix 提交3

--- src/main/java/com/henu/git/GitTest.java
+++ src/main/java/com/henu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println(123);
         System.out.println(1233);
         System.out.println("hot-fix commit");
+        System.out.println("hot-fix2");
     }
 }
